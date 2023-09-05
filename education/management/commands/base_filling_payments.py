5187ddb5c550662c963ff42f0b3d3743a55fd5d3
Anton Parshikov <tosha1703@gmail.com>
2023-09-05 17:03:46 +0300

    update command

--- education/management/commands/base_filling_payments.py
+++ education/management/commands/base_filling_payments.py
@@ -1,3 +1,5 @@
+import random
+
 from django.core.management.base import BaseCommand
 from users.models import User
 from datetime import date
@@ -8,20 +10,32 @@ class Command(BaseCommand):
     help = 'Create sample payment'
 
     def handle(self, *args, **options):
-        user = User.objects.first()
-        course = Course.objects.first()
-        lesson = Lesson.objects.first()
-        payment_date = date.today()
-        payment_amount = 10000
-        payment_method = 'Credit card'
+        users = User.objects.all()
+        courses = Course.objects.all()
+        lessons = Lesson.objects.all()
+        payment_methods = ['Credit card', 'PayPal', 'Bank transfer']
 
-        payment = Payment.objects.create(
-            user=user,
-            payment_date=payment_date,
-            paid_course=course,
-            paid_lesson=lesson,
-            payment_amount=payment_amount,
-            payment_method=payment_method
-        )
+        payments = []
 
-        self.stdout.write(self.style.SUCCESS(f'Successfully created payment {payment.id}'))
+        for i in range(3):
+            user = random.choice(users)
+            course = random.choice(courses)
+            lesson = random.choice(lessons)
+            payment_date = date.today()
+            payment_amount = random.randint(1000, 100000)
+            payment_method = random.choice(payment_methods)
+
+            payment = Payment(
+                user=user,
+                payment_date=payment_date,
+                paid_course=course,
+                paid_lesson=lesson,
+                payment_amount=payment_amount,
+                payment_method=payment_method
+            )
+
+            payments.append(payment)
+
+        Payment.objects.bulk_create(payments)
+
+        self.stdout.write(self.style.SUCCESS(f'Successfully created {len(payments)} payments'))
